eea64bb5e6abef9b19e1a1831908f18bef6a72a0
aamosa <44481508+aamosa@users.noreply.github.com>
2018-12-28 09:17:08 -0600

    Update Array_3.java

--- Array_3.java
+++ Array_3.java
@@ -4,14 +4,14 @@
  * Author: Omar Ahmed
  */
  public class Array_3 {
- 	/**
- 	 * Consider the leftmost and righmost appearances of some value in an array. We'll say that the "span" is the number of elements
- 	 * between the two inclusive. A single value has a span of 1. Return the largest span found in the given array.
- 	 * (Efficiency is not a priority.)
- 	 * maxSpan([1, 2, 1, 1, 3])       → 4
-	 * maxSpan([1, 4, 2, 1, 4, 1, 4]) → 6
-	 * maxSpan([1, 4, 2, 1, 4, 4, 4]) → 6
- 	 */
+    /**
+     * Consider the leftmost and righmost appearances of some value in an array. We'll say that the "span" is the number of elements
+     * between the two inclusive. A single value has a span of 1. Return the largest span found in the given array.
+     * (Efficiency is not a priority.)
+     * maxSpan([1, 2, 1, 1, 3])       → 4
+     * maxSpan([1, 4, 2, 1, 4, 1, 4]) → 6
+     * maxSpan([1, 4, 2, 1, 4, 4, 4]) → 6
+     */
      public static int maxSpan(int[] nums) {
         int max = 0, span = 0;
         if (nums.length > 0 && nums[0] == nums[nums.length-1]) {
@@ -23,11 +23,11 @@
                     span = j - i + 1;
                     if (span > max) {
                         max = span;
-		    }
+		            }
                 }
             }
         }
-	return max;
+	    return max;
      }
 
      /**
@@ -175,7 +175,6 @@
      */
     public int maxMirror(int[] nums) {
         int max = 0, count = 0, pos = 0;
-
         for (int i = 0; i < nums.length; i++) {
             for (int j = nums.length-1; j >= i; j--) {
                 if (i+pos <= nums.length-1 && j-pos >= 0 && nums[i+pos] == nums[j-pos]) {
