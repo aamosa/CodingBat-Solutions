7aa7c0e797bef14d73e9924e9a77fcc376383c33
aamosa <44481508+aamosa@users.noreply.github.com>
2018-12-28 09:15:50 -0600

    Update Array_3.java

--- Array_3.java
+++ Array_3.java
@@ -3,7 +3,6 @@
  * Section: Array-3
  * Author: Omar Ahmed
  */
-
  public class Array_3 {
  	/**
  	 * Consider the leftmost and righmost appearances of some value in an array. We'll say that the "span" is the number of elements
@@ -30,16 +29,15 @@
         }
 	return max;
      }
- 	
 
-	/**
-	 * Return an array that contains exactly the same numbers as the given array, but rearranged so that every 3 is immediately 
-   	 * followed by a 4. Do not move the 3's, but every other number may move. The array contains the same number of 3's and 4's, 
-   	 * every 3 has a number after it that is not a 3, and a 3 appears in the array before any 4.
-   	 * fix34([1, 3, 1, 4])          → [1, 3, 4, 1]
-   	 * fix34([1, 3, 1, 4, 4, 3, 1]) → [1, 3, 4, 1, 1, 3, 4]
-   	 * fix34([3, 2, 2, 4])          → [3, 4, 2, 2]
-	 */
+     /**
+     * Return an array that contains exactly the same numbers as the given array, but rearranged so that every 3 is immediately 
+     * followed by a 4. Do not move the 3's, but every other number may move. The array contains the same number of 3's and 4's, 
+     * every 3 has a number after it that is not a 3, and a 3 appears in the array before any 4.
+     * fix34([1, 3, 1, 4])          → [1, 3, 4, 1]
+     * fix34([1, 3, 1, 4, 4, 3, 1]) → [1, 3, 4, 1, 1, 3, 4]
+     * fix34([3, 2, 2, 4])          → [3, 4, 2, 2]
+     */
      public int[] fix34(int[] nums) {
         int previous = 0;
         for (int i = 0, j = 1; i < nums.length; i++) {
