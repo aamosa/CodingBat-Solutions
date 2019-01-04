12481e7b7567c30c79f0edc9925223f84c0c8e1e
aamosa <44481508+aamosa@users.noreply.github.com>
2019-01-04 07:45:03 -0600

    Update String_3.java

--- String_3.java
+++ String_3.java
@@ -55,9 +55,9 @@ public class String_3 {
      * Given a string, return true if the number of appearances of "is" anywhere in the string is equal to the number of appearances 
      * of "not" anywhere in the string (case sensitive).
      * 
-     * equalIsNot("This is not") 	       → false
-     * equalIsNot("This is notnot") 	   → true
-     * equalIsNot("noisxxnotyynotxisi")    → true
+     * equalIsNot("This is not")            → false
+     * equalIsNot("This is notnot")         → true
+     * equalIsNot("noisxxnotyynotxisi")     → true
      */
     public static boolean equalIsNot(String str) {
         int isCount = 0, notCount = 0, len = str.length();
