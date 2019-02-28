d2d192efcb5a884b305845c9728553c3038787e0
Omar Ahmed <44481508+aamosa@users.noreply.github.com>
2019-02-27 21:34:03 -0600

    Update Map_2.java

--- Map_2.java
+++ Map_2.java
@@ -126,7 +126,7 @@ public class Map_2 {
 	 * Given an array of strings, return a Map<String, Boolean> where each different string is a key and its value is true if that 
 	 * string appears 2 or more times in the array.
 	 * wordMultiple(["a", "b", "a", "c", "b"]) 	→ {"a": true, "b": true, "c": false}
-	 * wordMultiple(["c", "b", "a"])		→ {"a": false, "b": false, "c": false}
+	 * wordMultiple(["c", "b", "a"])			→ {"a": false, "b": false, "c": false}
 	 * wordMultiple(["c", "c", "c", "c"]) 		→ {"c": true}
 	 */
 	public static Map<String, Boolean> wordMultiple(String[] strings) {
