7656906020ee5a3170eb9ad179564b1b5cb10831
Omar Ahmed <44481508+aamosa@users.noreply.github.com>
2019-03-16 11:47:29 -0500

    Update ArrayTest.java

--- test/ArrayTest.java
+++ test/ArrayTest.java
@@ -13,15 +13,19 @@ import static org.junit.Assert.assertEquals;
 @RunWith(Parameterized.class)
 public class TestArray {
    
-   static int[] expectedValues = new int[] {3,3,0};
+   static int[] expectedValues = new int[] {3,3,0,1,2,1,2};
    static int index = 0;
 
    @Parameters
    public static Collection<int[]> testData() {
       return Arrays.asList(new int[][] {  
          {2, 1, 2, 3, 4}, 
-         {2, 2, 0},
-         {1, 3, 5}
+			{2, 2, 0},
+			{1, 3, 5},
+			{11, 9, 0, 1},
+			{2, 11, 9, 0},
+			{2},
+			{2, 5, 12}
       });
    }
    
